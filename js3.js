f2bd81e47ed84ed34ef49c4787e4a2d6b0f2a713
ricksr <sutirtharej@gmail.com>
2020-01-06 20:34:42 +0530

    str

--- js3.js
+++ js3.js
@@ -33,3 +33,4 @@ var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 // Change this line
 // In JavaScript, you can escape a quote from considering it as an 
 // end of string quote by placing a backslash (\) in front of the quote.
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a> ';
