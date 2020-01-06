d7d4c28cff8adab67505c8d8b306c6a6b29d93a9
ricksr <sutirtharej@gmail.com>
2020-01-07 02:17:29 +0530

    3 sstrs

--- js3.js
+++ js3.js
@@ -60,3 +60,14 @@ var myStr;
 myStr = "This is the first sentence. ";
 myStr += "This is the second sentence."
 
+//
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "ricksr";
+var myStr = "My name is " + myName + " and I am well!";
+
+
