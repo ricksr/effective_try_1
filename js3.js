585313a681d05f1f9eb84e957c9a67c133941775
ricksr <sutirtharej@gmail.com>
2020-01-06 17:23:35 +0530

    vars

--- js3.js
+++ js3.js
@@ -11,3 +11,21 @@ var ourVar = 19;
 
 // Only change code below this line
 var a = 9;
+
+// 
+
+// Initialize these three variables
+var a;
+var b;
+var c;
+
+// Do not change code below this line
+a = 5;
+b = 10;
+c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// 
