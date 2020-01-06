f22f5c1e620743c961d855579d8aee9937c8c1f2
ricksr <sutirtharej@gmail.com>
2020-01-06 17:54:44 +0530

    quotes inside a string quote

--- js4.js
+++ js4.js
@@ -11,3 +11,17 @@ var myDecimal = 5.7;
 // Only change code below this line
 
 var remainder = 5 % 3;
+//
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+//console.log(a,b,c);
+
+
