96f31ef13b59561a4d44bde10923e3fe2c6dc38f
ricksr <sutirtharej@gmail.com>
2020-01-07 02:33:32 +0530

    nested array

--- js6.js
+++ js6.js
@@ -3,3 +3,15 @@ var ourArray = ["John", 23];
 
 // Only change code below this line.
 var myArray = ["Hi", 5];
+// nested array
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+
+var myArray = [ourArray, "hi"];
+
+console.log(myArray);
+
+
