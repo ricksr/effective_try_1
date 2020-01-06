f608be2fc3268564e10615ca93d782b84573971a
ricksr <sutirtharej@gmail.com>
2020-01-06 17:59:01 +0530

    string

--- js3.js
+++ js3.js
@@ -29,3 +29,7 @@ b = b + 5;
 c = c + " String!";
 
 // 
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+// Change this line
+// In JavaScript, you can escape a quote from considering it as an 
+// end of string quote by placing a backslash (\) in front of the quote.
