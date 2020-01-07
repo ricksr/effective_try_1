c39e9df9290050ba768aa5d773dd46491830ab09
ricksr <sutirtharej@gmail.com>
2020-01-07 23:30:47 +0530

    passing-values-to-functions-with-arguments

--- js7.js
+++ js7.js
@@ -18,4 +18,18 @@ function ourReusableFunction() {
     console.log("Hi World");
   }
   reusableFunction();
+
+  //passing-values-to-functions-with-arguments
+
+
+// Example
+function ourFunctionWithArgs(a, b) {
+    console.log(a - b);
+  }
+  ourFunctionWithArgs(10, 5); // Outputs 5
   
+  // Only change code below this line.
+  function functionWithArgs(a, b) {
+    console.log(a + b);
+  }
+  functionWithArgs(10, 5);
