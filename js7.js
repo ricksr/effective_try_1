21a7ff350ae7006221b9b0e4ae57a32a150f57fa
ricksr <sutirtharej@gmail.com>
2020-01-07 23:24:30 +0530

    reusable functions

---
+++ js7.js
@@ -0,0 +1,21 @@
+// function
+
+function functionName() {
+    console.log("Hello World");
+  }
+
+// write resuable functions with JS
+
+// Example
+function ourReusableFunction() {
+    console.log("Heyya, World");
+  }
+  
+  ourReusableFunction();
+  
+  // Only change code below this line
+  function reusableFunction() {
+    console.log("Hi World");
+  }
+  reusableFunction();
+  
