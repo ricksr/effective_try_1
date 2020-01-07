72765010d9d98d2dab06f438d6b520fc206444ea
ricksr <sutirtharej@gmail.com>
2020-01-08 00:00:25 +0530

    global-scope-and-functions

--- js7.js
+++ js7.js
@@ -33,3 +33,26 @@ function ourFunctionWithArgs(a, b) {
     console.log(a + b);
   }
   functionWithArgs(10, 5);
+
+  //global-scope-and-functions
+
+  // Declare your variable here
+var myGlobal = 10;
+var oopsGlobal;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
