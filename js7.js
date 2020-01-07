123e2d3e360472520987164cd62a25557e1d31fe
ricksr <sutirtharej@gmail.com>
2020-01-08 02:54:08 +0530

    understanding-undefined-value-returned-from-a-function

--- js7.js
+++ js7.js
@@ -107,3 +107,19 @@ function minusSeven(num) {
   console.log ( timesFive(5) );
   console.log ( timesFive(2) );
   console.log ( timesFive(0) );
+
+  //understanding-undefined-value-returned-from-a-function
+
+  // Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+// Only change code above this line
+addThree();
+addFive();
